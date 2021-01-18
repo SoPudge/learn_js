4e7b2f7c09c2ff0e49eccd74deb97713f3bcbba2
SoPudge <abcd666@gmail.com>
2021-01-18 23:41:44 +0800

    20210118

--- miniprogram/test_program/pages/account/account.js
+++ miniprogram/test_program/pages/account/account.js
@@ -5,26 +5,15 @@ Page({
    */
   data: {
     //判断小程序的API，回调，参数，组件等是否在当前版本可用。
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
+    canIUse: wx.canIUse('button.open-type.getUserInfo'),
+    isRegisted: false,
+    avatarImg: "/images/empty_avatar.png"
   },
-
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function () {
-    // 查看是否授权
-    wx.getSetting({
-      success: function (res) {
-        if (res.authSetting['scope.userInfo']) {
-          wx.getUserInfo({
-            success: function (res) {
-              console.log(res.userInfo)
-              //用户已经授权过
-            }
-          })
-        }
-      }
-    })
+
   },
   bindGetUserInfo: function(e) {
     console.log(e.detail.userInfo)
