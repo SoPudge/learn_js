622a97d18a3adbf0c182b0c1625a0d301c3a0df0
SoPudge <abcd666@gmail.com>
2021-01-21 21:03:33 +0800

    20210122

--- miniprogram/test_program/pages/account/meat_log/meat_log.js
+++ miniprogram/test_program/pages/account/meat_log/meat_log.js
@@ -5,14 +5,32 @@ Page({
      * 页面的初始数据
      */
     data: {
-        array: [1,2,3,4,5,6,7]
+        // array: [1,2,3,4,5,6,7]
     },
 
     /**
      * 生命周期函数--监听页面加载
      */
     onLoad: function (options) {
-
+        var d = new Date()
+        var date_start_stamp = 1609502182000
+        var date_end_stamp = d.getTime()
+        wx.request({
+          url: 'http://corp.65536.io:8000/wxmeat/v1/meatlog',
+          data: {
+              openid: wx.getStorageSync('openid'),
+              date_start_stamp: date_start_stamp,
+              date_end_stamp: date_end_stamp
+          },
+          method: "GET",
+          success(res) {
+            //wx.setStorageSync('openid', res.data)
+            console.log(res.data)
+          },
+          fail(err){
+              //console.log(err)
+          }
+        })
     },
 
     /**
