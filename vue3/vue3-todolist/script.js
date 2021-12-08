9a11b13c929b4fb78ceb78f55de51da84c99a657
SoPudge <abcd666@gmail.com>
2021-12-08 16:50:20 +0800

    20211208-2

--- vue3/vue3-todolist/script.js
+++ vue3/vue3-todolist/script.js
@@ -1,9 +1,34 @@
-const app = {
+const app = Vue.createApp({
     //数据声明
-    data(){
-        return{
-            message: ''
+    data() {
+        return {
+            message: '',
+            todos: [
+                {
+                    id: 1,
+                    title: 'Do the dishes'
+                },
+                {
+                    id: 2,
+                    title: 'Take out the trash'
+                },
+                {
+                    id: 3,
+                    title: 'Mow the lawn'
+                }
+            ],
         }
     }
-}
-Vue.createApp(app).mount('#todo')
+})
+
+//checkbox + label组件
+app.component('todo-item',{
+    props: ['title'],
+    template: `
+        <li>
+        <input type="checkbox">
+        <label>{{title}}</label>
+        </li>
+    `
+})
+app.mount('#todo')
